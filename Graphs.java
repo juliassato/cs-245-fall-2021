80387be30a76d40eff0bcfe7e77d0f2b2610f8cb
julia <89543526+juliassato@users.noreply.github.com>
2021-11-15 14:05:31 -0800

    ueueueueu

--- Graphs.java
+++ Graphs.java
@@ -1,7 +1,7 @@
 /*
-	notes 11: graphs , breadth-first search
+	notes 11: graphs , breadth-first search, depth-first search
 	julia sato for cs-245 [fall 2021]
-	november 12, 2021
+	november 12, 2021, nov 15, 2021
 
 
 
@@ -12,14 +12,21 @@
 	> graph can be used for modelling contact tracing, spread of ideas, etc... network model
 		there are no parents or children in graphs -- association, not hierarchy
 		+ adjacency lists and matrices are the main representations
+		+ there is no graph package in standard java
+
 
 	
 	++ breadth-first search [BFS] ++ 
 	robots.txt 
 	- needs a source vertex, quque, and bool array to track visited/notvisited
 	- runtime O(|V|+|E|) 
-	- 
+	- how search engines work
 
+	++ depth-first search [DFS] ++ 
+	> the only difference between bfs and dfs is that DFS uses a stack and bfs uses a queue.
+	+ used for board games and things like that (lots of possibility, x starting position) 
+	
+	+++ visits in 'chains'... dfs is chains, bfs is layers
 
 */
 
